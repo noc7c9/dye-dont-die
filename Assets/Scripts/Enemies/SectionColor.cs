522ee5638b42adb7be5554dbfddaafd7ed7b5c15
Athir Saleem <noc7c9@gmail.com>
2019-12-14 15:52:18 +1100

    Start reimplementation of new goomba enemy

---
+++ Assets/Scripts/Enemies/SectionColor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Noc7c9.DyeDontDie
+{
+
+    public class SectionColor : MonoBehaviour
+    {
+
+        [SerializeField]
+        private SpriteRenderer solid = null;
+
+        [SerializeField]
+        private long colorIndex = 0;
+        [SerializeField]
+        private bool isWhite = true;
+
+        void Start()
+        {
+            solid.color = GetColor();
+        }
+
+        public bool IsWhite()
+        {
+            return isWhite;
+        }
+
+        public long GetColorIndex()
+        {
+            return colorIndex;
+        }
+
+        public Color GetColor()
+        {
+            if (isWhite)
+            {
+                return Color.white;
+            }
+            return ServiceLocator.WorldColorManager.GetColor(colorIndex);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Enemies/Tangibility.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+
+namespace Noc7c9.DyeDontDie
+{
+
+    public class Tangibility : MonoBehaviour
+    {
+
+        [SerializeField]
+        private SpriteRenderer solid = null;
+        [SerializeField]
+        private SpriteRenderer dotted = null;
+
+        [SerializeField]
+        private bool isTangible = true;
+
+        void Start()
+        {
+            solid.enabled = isTangible;
+            dotted.enabled = !isTangible;
+        }
+    }
+
+}
