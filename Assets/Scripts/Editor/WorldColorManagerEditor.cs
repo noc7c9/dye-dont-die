e3cdcd564c2b2dcacf189f8dd4e6114b7d0575b1
Athir Saleem <noc7c9@gmail.com>
2019-12-14 15:50:02 +1100

    Fix issue with building

---
+++ Assets/Scripts/Editor/WorldColorManagerEditor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Noc7c9.DyeDontDie
+{
+
+    [CustomEditor(typeof(WorldColorManager))]
+    public class WorldColorManagerEditor : Editor
+    {
+
+        public override void OnInspectorGUI()
+        {
+            if (DrawDefaultInspector())
+            {
+                (target as WorldColorManager).UpdateColors();
+            }
+        }
+
+    }
+
+}
--- Assets/Scripts/Managers/WorldColorManager.cs
+++ Assets/Scripts/Managers/WorldColorManager.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
-using UnityEditor;
 
-namespace Noc7c9.DyeDontDie {
+namespace Noc7c9.DyeDontDie
+{
 
-    public class WorldColorManager : MonoBehaviour {
+    public class WorldColorManager : MonoBehaviour
+    {
 
         public Camera cam;
 
@@ -12,58 +13,58 @@ namespace Noc7c9.DyeDontDie {
 
         private long activeColorIndex;
 
-        public long GetActiveColorIndex() {
+        public long GetActiveColorIndex()
+        {
             return activeColorIndex;
         }
 
-        public Color GetActiveColor() {
+        public Color GetActiveColor()
+        {
             return colors[activeColorIndex];
         }
 
-        public Color[] GetColors() {
+        public Color[] GetColors()
+        {
             return colors;
         }
 
-        public Color GetColor(long colorIndex) {
+        public Color GetColor(long colorIndex)
+        {
             return colors[colorIndex % colors.Length];
         }
 
         public event System.Action<long> ColorChanged;
-        void OnColorChanged() {
+        void OnColorChanged()
+        {
             var evt = ColorChanged;
-            if (evt != null) {
+            if (evt != null)
+            {
                 evt(activeColorIndex);
             }
         }
 
-        public void CycleColor() {
+        public void CycleColor()
+        {
             activeColorIndex = (activeColorIndex + 1) % colors.Length;
             OnColorChanged();
         }
 
-        void OnValidate() {
+        void OnValidate()
+        {
             activeColorIndex = (long)Mathf.Clamp(activeColorIndex, 0, colors.Length);
         }
 
-        void UpdateColors() {
-            if (cam != null) {
+        public void UpdateColors()
+        {
+            if (cam != null)
+            {
                 cam.backgroundColor = GetActiveColor();
             }
         }
 
-        void Update() {
+        void Update()
+        {
             UpdateColors();
-        }
-
-        [CustomEditor(typeof(WorldColorManager))]
-        public class WorldColorManagerEditor : Editor {
-
-            public override void OnInspectorGUI() {
-                if (DrawDefaultInspector()) {
-                    (target as WorldColorManager).UpdateColors();
-                }
-            }
-
         }
 
     }
