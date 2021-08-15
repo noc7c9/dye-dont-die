e80e62240d96537bbdc78f3822870c5a7e160dda
Athir Saleem <noc7c9@gmail.com>
2021-08-16 09:09:44 +1000

    Start goomba collidable

--- src/goomba/goomba.gd
+++ src/goomba/goomba.gd
@@ -11,11 +11,18 @@ var motion: Vector2 = Vector2.ZERO;
 
 onready var ray_left: RayCast2D = $ray_left
 onready var ray_right: RayCast2D = $ray_right
+onready var area_left: Area2D = $area_left
+onready var area_right: Area2D = $area_right
+onready var area_top: Area2D = $area_top
+
+func _ready():
+    area_left.connect('body_entered', self, '_on_body_entered')
+    area_right.connect('body_entered', self, '_on_body_entered')
+    area_top.connect('body_entered', self, '_on_body_entered')
 
 func _physics_process(delta):
     if not ray_right.is_colliding() or not ray_left.is_colliding():
         h_dir *= -1
-
 
     v_speed += gravity * delta
     v_speed = min(v_speed, max_fall_speed)
@@ -24,3 +31,6 @@ func _physics_process(delta):
     motion.x = h_dir * speed * delta
 
     motion = move_and_slide(motion, Vector2.UP)
+
+func _on_body_entered(body):
+    print('body entered ', body.is_in_group('player'))
