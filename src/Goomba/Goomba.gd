b29c8b1de85e8bdc242c14df5a25c9529c70c331
Athir Saleem <noc7c9@gmail.com>
2021-09-07 20:38:15 +1000

    Clean up goomba code

--- src/Goomba/Goomba.gd
+++ src/Goomba/Goomba.gd
@@ -2,38 +2,42 @@ class_name Goomba
 
 extends KinematicBody2D
 
-export var gravity: float = 1500
-export var speed: float = 5000
-export var max_fall_speed: float = 500
+const GRAVITY: float = 1500.0
+const SPEED: float = 5000.0
+const MAX_FALL_SPEED: float = 500.0
 
 var v_speed: float = 0
 var h_dir: float = 1
 
 var motion: Vector2 = Vector2.ZERO;
 
-onready var ray_left: RayCast2D = $ray_left
-onready var ray_right: RayCast2D = $ray_right
-onready var area_left: Area2D = $area_left
-onready var area_right: Area2D = $area_right
-onready var area_top: Area2D = $area_top
+onready var ray_left: RayCast2D = $RayLeft
+onready var ray_right: RayCast2D = $RayRight
+onready var area_left: Area2D = $AreaLeft
+onready var area_right: Area2D = $AreaRight
+onready var area_top: Area2D = $AreaTop
 
 func _ready():
-    area_left.connect('body_entered', self, '_on_body_entered')
-    area_right.connect('body_entered', self, '_on_body_entered')
-    area_top.connect('body_entered', self, '_on_body_entered')
+    assert(area_left.connect('body_entered', self, '_on_body_side_entered') == OK)
+    assert(area_right.connect('body_entered', self, '_on_body_side_entered') == OK)
+    assert(area_top.connect('body_entered', self, '_on_body_top_entered') == OK)
 
 func _physics_process(delta):
     if not ray_right.is_colliding() or not ray_left.is_colliding():
         h_dir *= -1
 
-    v_speed += gravity * delta
-    v_speed = min(v_speed, max_fall_speed)
+    v_speed += GRAVITY * delta
+    v_speed = min(v_speed, MAX_FALL_SPEED)
 
     motion.y = v_speed
-    motion.x = h_dir * speed * delta
+    motion.x = h_dir * SPEED * delta
 
     motion = move_and_slide(motion, Vector2.UP)
 
-func _on_body_entered(body):
-    # print('body entered ', body.is_in_group('player'))
-    pass
+func _on_body_side_entered(body):
+    if not body.is_in_group('player'): return
+    print('body side entered')
+
+func _on_body_top_entered(body):
+    if not body.is_in_group('player'): return
+    print('body top entered')
